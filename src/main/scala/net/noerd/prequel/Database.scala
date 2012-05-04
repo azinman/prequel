ac6be7c7f9f8a90ca7508f99f977923bcfb3c3df
Aaron Zinman <aaron@azinman.com>
2012-05-04 15:35:37 -0400

    KeyedObjectPoolFactory uses generics on newer commons-pool build, but is unnecessary here.

--- src/main/scala/net/noerd/prequel/Database.scala
+++ src/main/scala/net/noerd/prequel/Database.scala
@@ -14,52 +14,52 @@ import org.apache.commons.pool.impl.GenericObjectPool
 
 
 private [prequel] object ConnectionPools {
-    
+
     private val pools: MMap[ DatabaseConfig, PoolingDataSource ]= new HashMap
     private val UserProperty = "user"
-    private val PasswordProperty = "password"    
-    
+    private val PasswordProperty = "password"
+
     def nbrOfPools = pools.size
-    
+
     def getOrCreatePool( config: DatabaseConfig ): PoolingDataSource = {
-        pools.get( config ).getOrElse { 
-            val connectionPool = new GenericObjectPool( 
-                null, config.poolConfig.toGenericObjectPoolConfig 
+        pools.get( config ).getOrElse {
+            val connectionPool = new GenericObjectPool(
+                null, config.poolConfig.toGenericObjectPoolConfig
             )
-            val connectionProperties = mapAsProperties( Map( 
-                UserProperty -> config.username, 
-                PasswordProperty -> config.password 
+            val connectionProperties = mapAsProperties( Map(
+                UserProperty -> config.username,
+                PasswordProperty -> config.password
             ) )
-            val connectionFactory = new DriverManagerConnectionFactory( 
-                config.jdbcURL, connectionProperties 
+            val connectionFactory = new DriverManagerConnectionFactory(
+                config.jdbcURL, connectionProperties
             )
             val defaultReadonly = false
             val defaultAutoCommit = false
-            val stmtPoolFactory: KeyedObjectPoolFactory = null
+            //val stmtPoolFactory: KeyedObjectPoolFactory = null
             val validationQuery = "select 1"
             val poolableConnectionFactory = new PoolableConnectionFactory(
-                connectionFactory, connectionPool, stmtPoolFactory, validationQuery, 
+                connectionFactory, connectionPool, null, validationQuery,
                 defaultReadonly, defaultAutoCommit, config.isolationLevel.id
             )
             val dataSource: PoolingDataSource = {
                 new PoolingDataSource( connectionPool )
             }
-            
+
             pools.synchronized {
                 pools += (( config, dataSource ))
             }
-            
+
             dataSource
         }
     }
-    
+
     // Conversion method to deal with the nasty java.util.Properties class
     private def mapAsProperties( aMap: Map[ String, String ] ): Properties = {
         val properties = new Properties
         aMap.map( pair => properties.setProperty( pair._1, pair._2 ) )
         properties
     }
-    
+
     // Used during testing
     private[prequel] def reset(): Unit = pools.clear
 }
